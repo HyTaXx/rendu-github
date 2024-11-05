7b6db6616448f52731835670dd8fdfc3a301031a
Armand Nobre <armandnobre@MacBook-Pro-de-Armand.local>
2024-11-05 10:55:48 +0100

    :chore(tests): add Jest config and setup files

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+module.exports = {
+    transform: {
+      "^.+\\.tsx?$": "babel-jest"
+    },
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+    testEnvironment: "jest-environment-jsdom",
+  };
+  
